0defa7a1ae5b962aabb7194599b4ec9cba6a15bd
Deserie Murembeni <deserie.dev@gmail.com>
2021-09-10 20:43:19 +0200

    edit output

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.status(200).json("SCA Cloud School Week 5: CI/CD Tools");
+  res.status(200).json("SCA Cloud School Week 5: CI/CD Tools - Jenkins, CircleCI, Travis");
 });
 
 module.exports = app.listen(process.env.PORT || 4000, () =>
