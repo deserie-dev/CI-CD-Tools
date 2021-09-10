465a30e2354bd7e4652b71fdc54bca20229ca28f
Deserie Murembeni <deserie.dev@gmail.com>
2021-09-10 20:04:57 +0200

    add: test

---
+++ __test__/index.test.js
@@ -0,0 +1,11 @@
+const supertest = require("supertest");
+const app = require("../index");
+
+test("GET route", async () => {
+    await supertest(app)
+        .get("/")
+        .expect(200)
+        .then(async (res) => {
+            expect(res.body).toBe("Hello world")
+        })
+});
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
-  res.status(200).json("SCA Cloud School Week 5.");
+  res.status(200).json("Hello world");
 });
 
 module.exports = app.listen(process.env.PORT || 4000, () =>
