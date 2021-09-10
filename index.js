8552de8196e9efd76d71b25ef0348c0498957a6b
Deserie Murembeni <deserie.dev@gmail.com>
2021-09-10 18:29:21 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json("SCA Cloud School Week 5.");
+});
+
+module.exports = app.listen(process.env.PORT || 4000, () =>
+  console.log(`Running on http://localhost:4000`)
+);
