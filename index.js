6f78862e1b3e36ece6bde43bfac8de4ed8428491
Deserie Murembeni <deserie.dev@gmail.com>
2021-09-11 19:42:13 +0200

    fix errors

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-
-const { PORT = 3000 } = process.env;
 const app = express();
 
 app.get('/', (req, res) => {
   res.status(200).json('SCA Cloud School Week 5: CI/CD Tools - Jenkins, CircleCI, TravisCI');
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}`);
-});
+module.exports = app.listen(process.env.PORT || 4000, () =>
+  console.log(`Running on http://localhost:4000`)
+);
