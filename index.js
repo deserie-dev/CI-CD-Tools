2afd1c5187c43644b6cafe91ae0890c642c883b9
Deserie Murembeni <deserie.dev@gmail.com>
2021-09-11 17:02:09 +0200

    fix: index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
+
+const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.status(200).json("SCA Cloud School Week 5: CI/CD Tools - Jenkins, CircleCI, TravisCI");
+app.get('/', (req, res) => {
+  res.status(200).json('SCA Cloud School Week 5: CI/CD Tools - Jenkins, CircleCI, TravisCI');
 });
 
-module.exports = app.listen(process.env.PORT || 4000, () =>
-  console.log(`Running on http://localhost:4000`)
-);
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
