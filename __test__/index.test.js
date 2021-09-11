6fca522d681eb2f6faae57c3fc6e7e7dc5ed08b1
Deserie Murembeni <deserie.dev@gmail.com>
2021-09-11 20:12:48 +0200

    edit index.test.js

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -6,6 +6,10 @@ test("GET route", async () => {
         .get("/")
         .expect(200)
         .then(async (res) => {
-            expect(res.body).toBe("SCA Cloud School Week 5: CI/CD Tools - Jenkins, CircleCI, TravisCI")
+            expect(res.body).toBe("SCA Cloud School Week 5: CI/CD Tools - Jenkins, CircleCI, TravisCI")    
         })
+});
+
+afterAll(async () => {
+  await app.close();
 });
